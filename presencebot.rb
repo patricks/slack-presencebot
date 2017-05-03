19816e10b1cc39da390fead20a35026fc4d5e7f6
Patrick <patrick@helmsdeep.at>
2017-05-03 20:04:31 +0200

    updated bot

--- presencebot.rb
+++ presencebot.rb
@@ -9,16 +9,39 @@ end
 
 @presence_channel = nil
 
+def is_lunch_emoji(status_emoji)
+  lunch_emojis = [
+    ':fork_and_knife:',
+    ':meat_and_bone:',
+    ':hotdog:',
+    ':hamburger:',
+    ':spaghetti:',
+    ':sushi:',
+    ':rice:',
+    ':bento:',
+    ':pizza:'
+  ]
+  
+  lunch_emojis.include? status_emoji
+end
+
 ##
-# Check if the the status is a lunch status.
+# Check if the status is a lunch status.
 def is_lunch_status(status_text, status_emoji)
-  case status_emoji
-  when ':fork_and_knife:' then
-    return true
-  end
+  return true if is_lunch_emoji(status_emoji)
   
   case status_text
   when /[M,m]ahlzeit/ then
+    return true
+  end
+  
+  false
+end
+
+##
+# Check if the status is a empty status.
+def is_home_status(status_text, status_emoji)
+  if status_text.empty? && status_emoji.empty?
     return true
   end
   
@@ -29,20 +52,28 @@ client = Slack::RealTime::Client.new
 
 client.on :hello do
   puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
-end
-
-client.on :message do |data|
-  puts "Using channel: #{data.channel}"
-  @presence_channel = data.channel
+  
+  client.channels.each do |channel|
+    channel_data = channel[1]
+    return if channel_data.nil?
+    
+    if channel_data.is_member == true
+      puts "Using channel - name: #{channel_data.name} id: #{channel_data.id}"
+      @presence_channel = channel_data.id
+    end
+  end
 end
 
 client.on :user_change do |data|
   status_channel = @presence_channel
+  return if status_channel.nil?
   
   client.typing channel: status_channel
   
   if is_lunch_status(data.user.profile.status_text, data.user.profile.status_emoji)
     client.message channel: status_channel, text: "#{data.user.profile.first_name} ist jetzt Mittagessen. 🍴"
+  elsif is_home_status(data.user.profile.status_text, data.user.profile.status_emoji)
+    client.message channel: status_channel, text: "#{data.user.profile.first_name} ist jetzt zu Hause."
   else
     client.message channel: status_channel, text: "#{data.user.profile.first_name}'s neuer Status: #{data.user.profile.status_text}"
   end
